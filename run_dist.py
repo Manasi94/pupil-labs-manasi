8436bb82effe5901acdb4cd3b450f4146060a2bd
Manasi94 <manasimuglikar@gmail.com>
2015-06-03 18:41:51 +0530

    python code to run dist_pts_ellipse function

---
+++ run_dist.py
@@ -0,0 +1,8 @@
+import numpy as np
+from dist_pts_ellipse import dist_pts_ellipse
+ellipse = ((0,0),(20,10),0)
+pts = (np.random.rand(200,2))*10
+from timeit import Timer
+t = Timer(lambda: dist_pts_ellipse(ellipse,pts))
+print "Time required for numpy"
+print t.timeit(number=5)
