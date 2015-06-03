db52264da7d80e9e0400515f31e3eacc1b0576af
Manasi94 <manasimuglikar@gmail.com>
2015-06-03 18:44:39 +0530

    removed errors

--- run_dist.py
+++ run_dist.py
@@ -4,5 +4,5 @@ ellipse = ((0,0),(20,10),0)
 pts = (np.random.rand(200,2))*10
 from timeit import Timer
 t = Timer(lambda: dist_pts_ellipse(ellipse,pts))
-print "Time required for numpy"
+print "Time required for cython"
 print t.timeit(number=5)
