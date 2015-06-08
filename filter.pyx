b5a08fd215fbce932de4c0a8afd251659b88c428
Manasi94 <manasimuglikar@gmail.com>
2015-06-08 16:31:21 +0530

    added filter.pyx still has errors

--- filter.pyx
+++ filter.pyx
@@ -16,13 +16,10 @@ cdef struct eye_t:
     int h
 
 
-cdef float area(const float *img,point_t size,point_t start,point_t end,point_t offset):
-    #// use like in numpy including start excluding end
-    #// 0-based indecies
-    #// this is because the integral image has w+1 and h+1
+def area(const float *img,point_t size,point_t start,point_t end,point_t offset):
     return    (img[(offset.r + end.r  ) * size.c + offset.c + end.c]+ img[(offset.r + start.r) * size.c + offset.c + start.c]- img[(offset.r + start.r) * size.c + offset.c + end.c]- img[(offset.r + end.r  ) * size.c + offset.c + start.c])
 
-cdef eye_t make_eye(int h):
+cpdef eye_t make_eye(int h):
     cdef int w = 3*h
     cdef eye_t eye
     eye.h = h
@@ -38,12 +35,7 @@ cdef eye_t make_eye(int h):
     eye.w_half = w/2
     return eye
 
-cdef filter(const float *img, const int rows, const int cols, int * x_pos,int *y_pos,int *width, int min_w,int max_w,float *response):
-# Algorithm based on:
-# Robust real-time pupil tracking in highly off-axis images
-# Lech Świrski Andreas Bulling Neil A. Dodgson
-# Computer Laboratory, University of Cambridge, United Kingdom
-# Eye Tracking Research & Applications 2012
+cpdef void filter(const float *img, const int rows, const int cols, int * x_pos,int *y_pos,int *width, int min_w,int max_w,):
 
     cdef point_t img_size = {rows,cols}
     cdef int min_h = min_w/3
@@ -56,22 +48,17 @@ cdef filter(const float *img, const int rows, const int cols, int * x_pos,int *y
     cdef int step = 5
     cdef eye_t eye
     cdef point_t offset
-    cdef float b
-    cdef float a
-    cdef float c
-    cdef float d
+    cdef float a,b,c,d
 
     for h in range(min_h,max_h,h_step):
       eye = make_eye(h)
       for i in range(0,rows-eye.w,step):
         for j in range(0,cols-eye.w,step):
+          #// printf("|%2.0f",img[w,step):
           #// printf("|%2.0f",img[i * cols + j]);
           offset = {i,j}
-          b = area(img,img_size,eye.outer.s,eye.outer.e,offset)
-          a = eye.outer.f
-          c = eye.inner.f
-          d = area(img,img_size,eye.inner.s,eye.inner.e,offset)
-          float response = a
+
+          response = eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset)+eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset)
           #// printf("| %5.2f ",response)
           if(response  > best_response):
             #// printf("!");
