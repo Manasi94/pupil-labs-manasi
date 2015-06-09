369a5b87facd5e41147a2b31777577c6b001be91
Manasi94 <manasimuglikar@gmail.com>
2015-06-09 23:02:17 +0530

    python code to run filter.pyx

--- eye_filter.py
+++ eye_filter.py
@@ -1,19 +1,17 @@
 from numpy.ctypeslib import ndpointer
+from fill import fil
 
 
-def eye_filter(integral,min_w=10,max_w=100):
-    rows, cols = integral.shape[0],integral.shape[1]
-    cdef int x, y, w
-    cdef float response
-    min_w,max_w = int(min_w),int(max_w)
-
-    return x.value,y.value,w.value,response.value
+#def eye_filter(integral,min_w=10,max_w=100):
+#    rows, cols = integral.shape[0],integral.shape[1]
+#    min_w,max_w = int(min_w),int(max_w)
+#    filter(integral)
 
 ### Debugging
 if __name__ == '__main__':
     import numpy as np
     import cv2
-    img = np.ones((1000,1000),dtype= uint8)
+    img = np.ones((1000,1000),dtype= np.uint8)
     # img = np.random.rand((100))
     # img = img.reshape(10,-1)
     # img *=20;
@@ -23,4 +21,4 @@ if __name__ == '__main__':
     # print img
     integral = cv2.integral(img)
     integral =  np.array(integral,dtype=np.float32)
-    print eye_filter(integral)
+    print fil(integral)
