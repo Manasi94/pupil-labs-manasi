ee76233a28bf82c01125086b0d10b1c81b387552
Manasi94 <manasimuglikar@gmail.com>
2015-06-12 23:06:24 +0530

    updated

--- filter.pyx
+++ filter.pyx
@@ -21,14 +21,13 @@ cdef struct eye_t:
     int h
 
 
-cpdef float area(np.ndarray[np.float32_t, ndim=2] img,point_t size,point_t start,point_t end,point_t offset):
+cdef float area(np.ndarray[np.float32_t, ndim=2] img,point_t size,point_t start,point_t end,point_t offset):
   return    img[offset.r + end.r ,offset.c + end.c]\
         + img[offset.r + start.r, offset.c + start.c]\
         - img[offset.r + start.r, offset.c + end.c]\
         - img[offset.r + end.r,offset.c + start.c]
-#    return    (img[(offset.r + end.r) * size.c + offset.c + end.c] + img[(offset.r + start.r) * size.c + offset.c + start.c] - img[(offset.r + start.r) * size.c + offset.c + end.c]- img[(offset.r + end.r  ) * size.c + offset.c + start.c])
 
-cpdef eye_t make_eye(int h):
+cdef eye_t make_eye(int h):
     cdef int w = 3*h
     cdef eye_t eye
     eye.h = h
@@ -60,59 +59,50 @@ cpdef fil(np.ndarray[np.float32_t, ndim=2] img, int min_w=10,int max_w=100):
     cdef point_t offset
     cdef int x_pos,y_pos,width
     cdef float response = 0
+    cdef float a,c,
+    cdef point_t b,d,e,f
 
-    for h in range(min_h,max_h,h_step):
+    for h in xrange(min_h,max_h,h_step):
       eye = make_eye(h)
-      for i in range(0,img_size.r-eye.w,step):
-        for j in range(0,img_size.c-eye.w,step):
-          #// printf("|%2.0f",img[w,step):
-          #// printf("|%2.0f",img[i * cols + j]);
+    #   a = eye.outer.f
+    #   c = eye.inner.f
+    #   b = eye.outer.s
+    #   d = eye.outer.e
+    #   e = eye.inner.s
+    #   f = eye.inner.e
+      for i in xrange(0,img_size.r-eye.w,step):
+        for j in xrange(0,img_size.c-eye.w,step):
           offset = point_t(i,j)
-        #   print "i"
-        #   print i
-        #   print "j"
-        #   print j
-          response = eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset)+eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset)
-
+          response =eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset)\
+          +eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset)
           if(response  > best_response):
-            #// printf("!");
             best_response = response
             best_pos = point_t(i,j)
-            #// printf("%i %i", (int)best_pos.r,(int)best_pos.c);
-            best_h = eye.h
+            best_h = h
 
 
     cdef point_t window_lower = point_t(max(0,best_pos.r-step+1),max(0,best_pos.c-step+1))
     cdef point_t window_upper = point_t(min(img_size.r,best_pos.r+step),min(img_size.c,best_pos.c+step))
-    for h in range(best_h-h_step+1,best_h+h_step):
+    for h in xrange(best_h-h_step+1,best_h+h_step):
             eye = make_eye(h)
-
-            for i in range(window_lower.r,min(window_upper.r,img_size.r-eye.w)) :
-                for j in range(window_lower.c,min(window_upper.c,img_size.c-eye.w)):
-
-                    # printf("|%2.0f",img[i * cols + j]);
+            # a = eye.outer.f
+            # c = eye.inner.f
+            # b = eye.outer.s
+            # d = eye.outer.e
+            # e = eye.inner.s
+            # f = eye.inner.e
+            for i in xrange(window_lower.r,min(window_upper.r,img_size.r-eye.w)) :
+                for j in xrange(window_lower.c,min(window_upper.c,img_size.c-eye.w)):
                     offset = point_t(i,j)
-                    response =  eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset)+eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset);
-                    # ikiuprintf("| %5.2f ",response);
+                    response =eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset)\
+                    +eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset)
                     if(response > best_response):
-                        #// printf("!");
                         best_response = response
                         best_pos = point_t(i,j)
-                        #// printf("%i %i", (int)best_pos.r,(int)best_pos.c);
-                        best_h = eye.h
+                        best_h = h
 
-
-                #// printf("\n");
-
-
-
-    #// point_t start = {0,0};
-    #// point_t end = {1,1};
-    #// printf("FULL IMG SUM %1.0f\n",img[(img_size.r-1) * img_size.c + (img_size.c-1)] );
-    #// printf("AREA:%f\n",area(img,img_size,start,end,(point_t){0,0}));
     x_pos = best_pos.r
     y_pos = best_pos.c
     width = best_h*3
     response = best_response
-    #print x_pos,y_pos,width,response
     return x_pos,y_pos,width,response
