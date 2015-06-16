17a40787fb1bd0c73fb5a65bd1341d7f188b6376
Manasi94 <manasimuglikar@gmail.com>
2015-06-16 23:48:43 +0530

    modifies and updated

--- filter.pyx
+++ filter.pyx
@@ -1,7 +1,4 @@
-import numpy as np
-cimport numpy as np
-DTYPE = np.float32
-ctypedef np.float_t DTYPE_t
+cimport cython
 
 cdef struct point_t :
    int    r
@@ -21,21 +18,28 @@ cdef struct eye_t:
     int h
 
 
-cdef float area(np.ndarray[np.float32_t, ndim=2] img,point_t size,point_t start,point_t end,point_t offset):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef inline float area(int[:,::1] img,point_t size,point_t start,point_t end,point_t offset):
   return    img[offset.r + end.r ,offset.c + end.c]\
         + img[offset.r + start.r, offset.c + start.c]\
         - img[offset.r + start.r, offset.c + end.c]\
         - img[offset.r + end.r,offset.c + start.c]
 
-cdef eye_t make_eye(int h):
+@cython.cdivision(True)
+cdef inline eye_t make_eye(int h) nogil:
     cdef int w = 3*h
     cdef eye_t eye
     eye.h = h
     eye.w = w
-    eye.outer.s = point_t(0,0)
-    eye.outer.e = point_t(w,w)
-    eye.inner.s = point_t(h,h)
-    eye.inner.e = point_t(h+h,h+h)
+    eye.outer.s.r = 0
+    eye.outer.s.c = 0
+    eye.outer.e.r = w
+    eye.outer.e.c = w
+    eye.inner.s.r = h
+    eye.inner.s.c = h
+    eye.inner.e.r = h+h
+    eye.inner.e.c = h+h
     eye.inner.a = h*h
     eye.outer.a = w*w
     eye.outer.f =  1.0/eye.outer.a
@@ -43,15 +47,17 @@ cdef eye_t make_eye(int h):
     eye.w_half = w/2
     return eye
 
-cpdef fil(np.ndarray[np.float32_t, ndim=2] img, int min_w=10,int max_w=100):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def fil(int[:,::1] img, int min_w=10,int max_w=100):
     cdef point_t img_size
-    img_size.r  = img.shape[0]
-    img_size.c = img.shape[1]
+    img_size.r =  img.shape[0]
+    img_size.c =  img.shape[1]
     cdef int min_h = min_w/3
     cdef int max_h = max_w/3
-    cdef int h, i, j
+    cdef int h=0, i=0, j=0
     cdef float best_response = -10000
-    cdef point_t best_pos = point_t(0,0)
+    cdef point_t best_pos
     cdef int best_h = 0
     cdef int h_step = 4
     cdef int step = 5
@@ -62,47 +68,50 @@ cpdef fil(np.ndarray[np.float32_t, ndim=2] img, int min_w=10,int max_w=100):
     cdef float a,c,
     cdef point_t b,d,e,f
 
-    for h in xrange(min_h,max_h,h_step):
+
+
+    for h in range(min_h,max_h,h_step):
+    #for h from min_h <= h < max_h by h_step:
       eye = make_eye(h)
-    #   a = eye.outer.f
-    #   c = eye.inner.f
-    #   b = eye.outer.s
-    #   d = eye.outer.e
-    #   e = eye.inner.s
-    #   f = eye.inner.e
-      for i in xrange(0,img_size.r-eye.w,step):
-        for j in xrange(0,img_size.c-eye.w,step):
-          offset = point_t(i,j)
-          response =eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset)\
-          +eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset)
+      #for i in range(0,img_size.r-eye.w,step): #step is slow
+      for i from 0 <= i < img_size.r-eye.w by step:
+        #for j in range(0,img_size.c-eye.w,step): #step is slow
+        for j from 0 <= j < img_size.c-eye.w by step:
+
+          offset.r = i
+          offset.c = j
+
+          response = eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset) + eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset)
           if(response  > best_response):
             best_response = response
-            best_pos = point_t(i,j)
+            best_pos.r = i
+            best_pos.c = j
             best_h = h
 
 
-    cdef point_t window_lower = point_t(max(0,best_pos.r-step+1),max(0,best_pos.c-step+1))
-    cdef point_t window_upper = point_t(min(img_size.r,best_pos.r+step),min(img_size.c,best_pos.c+step))
-    for h in xrange(best_h-h_step+1,best_h+h_step):
-            eye = make_eye(h)
-            # a = eye.outer.f
-            # c = eye.inner.f
-            # b = eye.outer.s
-            # d = eye.outer.e
-            # e = eye.inner.s
-            # f = eye.inner.e
-            for i in xrange(window_lower.r,min(window_upper.r,img_size.r-eye.w)) :
-                for j in xrange(window_lower.c,min(window_upper.c,img_size.c-eye.w)):
-                    offset = point_t(i,j)
-                    response =eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset)\
-                    +eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset)
-                    if(response > best_response):
-                        best_response = response
-                        best_pos = point_t(i,j)
-                        best_h = h
+    cdef point_t window_lower
+    window_lower.r = max(0,best_pos.r-step+1)
+    window_lower.c = max(0,best_pos.c-step+1)
+
+
+    cdef point_t window_upper
+    window_upper.r = min(img_size.r,best_pos.r+step)
+    window_upper.c = min(img_size.c,best_pos.c+step)
+
+    for h in range(max(3,best_h-h_step+1),best_h+h_step):
+        eye = make_eye(h)
+        for i in range(window_lower.r,min(window_upper.r,img_size.r-eye.w)) :
+            for j in range(window_lower.c,min(window_upper.c,img_size.c-eye.w)):
+                offset.r = i
+                offset.c = j
+                response = eye.outer.f*area(img,img_size,eye.outer.s,eye.outer.e,offset) + eye.inner.f*area(img,img_size,eye.inner.s,eye.inner.e,offset)
+                if(response > best_response):
+                    best_response = response
+                    best_pos.r = i
+                    best_pos.c = j
+                    best_h = h
 
     x_pos = best_pos.r
     y_pos = best_pos.c
     width = best_h*3
-    response = best_response
-    return x_pos,y_pos,width,response
+    return x_pos,y_pos,width,best_response
