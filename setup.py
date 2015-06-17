1a94225eae5392c2f9b7f6b86355c97ac4443fd0
Manasi94 <manasimuglikar@gmail.com>
2015-06-17 23:41:40 +0530

    setup file for square marker detect

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
-	ext_modules = cythonize("find_curve.pyx"),
+	ext_modules = cythonize("square_marker_detect.pyx"),
+    include_dirs = [numpy.get_include()]
 
 )
