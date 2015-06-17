42938863b719e52d8e871f986aeb3f27a92aa186
Manasi94 <manasimuglikar@gmail.com>
2015-06-17 23:38:58 +0530

    benchmarked for eye filter

--- eye_filter.py
+++ eye_filter.py
@@ -13,19 +13,31 @@ if __name__ == '__main__':
     import cv2
     from timeit import Timer
 
+    from c_methods import eye_filter
 
-    img = np.ones((1000,1000),dtype= np.uint8)
-    # img = np.random.rand((100))
-    # img = img.reshape(10,-1)
-    # img *=20;
-    # img = np.array(img,dtype = c_uint8)
+    img = np.ones((640,480),dtype= np.uint8)
+    img = np.random.rand(img.shape[0]*img.shape[1]).reshape(img.shape)
+    img *=20
+    img = np.array(img,dtype = np.uint8)
     # img +=20
-    img[50:80,100:130] = 0
+    img[50:52,100:102] = 0
     # print img
     integral = cv2.integral(img)
-    integral =  np.array(integral,dtype=np.float32)
+    # integrabbl =  np.array(integral,dtype=np.float32)
     #print integral.size
     print fil(integral)
+
+
+    t = Timer(lambda: cv2.integral(img))
+    print "Time required for integral"
+    print t.timeit(number=1000)
+
     t = Timer(lambda: fil(integral))
     print "Time required for cython"
-    print t.timeit(number=5)
+    print t.timeit(number=1000)
+
+    # int 0.355319976807
+    print eye_filter(np.float32(integral))
+    t = Timer(lambda: eye_filter(np.float32(integral)))
+    print "Time required for ctypes"
+    print t.timeit(number=1000)
